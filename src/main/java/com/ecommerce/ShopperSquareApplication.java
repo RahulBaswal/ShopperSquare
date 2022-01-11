cb6d618e69d699a44c4399d913f3c1612e7a5c6e
Rahul Baswal <rb.rahulbaswal@gmail.com>
2022-01-11 21:26:55 +0530

    First commit

---
+++ src/main/java/com/ecommerce/ShopperSquareApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopperSquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopperSquareApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ecommerce/dto/ProductDTO.java
@@ -0,0 +1,18 @@
+package com.ecommerce.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+	private long id;
+
+	private String name;
+
+	private int categoryId;
+
+	private double price;
+
+	private String description;
+
+	private String imageName;
+}
